bff4b450cd5636c9ce697f230fc985fbc28fd6b0
Nils Perret <nils-frederic.perret@bos.no>
2022-10-07 09:33:34 +0200

    Remove quarkus template

--- template/kotlin-quarkus-native/function/src/test/kotlin/com/openfaas/function/NativeExampleResourceIT.kt
+++
@@ -1,8 +0,0 @@
-package com.openfaas.function
-
-import io.quarkus.test.junit.NativeImageTest
-import org.junit.jupiter.api.Disabled
-
-@Disabled
-@NativeImageTest
-class NativeExampleResourceIT : HandlerTest()
